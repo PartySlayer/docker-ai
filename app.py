2757120eb59bca62035f2f819edcd2f0a84ee210
PartySlayer <squadrittichristian@gmail.com>
2025-11-08 00:44:26 +0100

    repo initialized, first endpoint to handle communication

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from pizza_class import FoodClassification
+import os
+
+app = Flask(__name__)
+
+@app.route("/classify", methods=["POST"])
+def classify_image():
+    """
+    this endpoint will take care of the communication between nn and docker
+    """
+    if "image" not in request.files:
+        return "No Image :(", 400
+    
+    img_directory = "./input_images"
+    if not os.path.exists(img_directory):
+        os.mkdir(img_directory)
+
+    img_file = request.files["image"]
+    img_path = img_directory + "/" + img_file
+    img_file.save(img_path)
+
+    
---
+++ pizza_class.py
@@ -0,0 +1,2 @@
+def pizza_classification():
+    
